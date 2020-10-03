8509df1e297315ed862764ca677f650e70f18532
Estibaliz Barato <estibalizbarato@gmail.com>
2020-10-03 17:33:02 +0200

    inital commit & fail exercise 1

---
+++ Module-2-leson-3-exercise-1/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// avatar por defecto
+const DEFAULT_AVATAR = "http://placehold.it/300x300";
+// avatar que eligió el usuario al registrarse
+let userAvatar = "http://www.fillmurray.com/300/300";
+
+// Alicia-No consigo hallar solución a este ejercicio. He probado todas las maneras que se me han ocurrido y no logro resolvelo (aquí solo hay unas poquitas)
+
+console.log;
+userAvatar.setAttribute = "http://www.fillmurray.com/300/300";
+
+// userAvatar = document.images[userAvatar]."src";
+
+// userAvatar.setAttribute("http://www.fillmurray.com/300/300");
+
+// userAvatar.setAttribute("src", "http://www.fillmurray.com/300/300");
+
+// Intento-2
+// userAvatar.setAttribute("src",userAvatar);
+
+// Intento-3
+// userAvatar.setAttribute("http://www.fillmurray.com/300/300", userAvatar);
+
+// Intento-4
+// userAvatar.document.querySelector("img");
+// img.setAtribute("src", userAvatar);
