61ce04d05c3e1f9c0002f3bf9c194cd456d4b0e1
Estibaliz Barato <estibalizbarato@gmail.com>
2020-10-04 17:01:08 +0200

    add exercises to leson 2.1

---
+++ Module-2-leson-2-exercise-1to8/main.js
@@ -0,0 +1,39 @@
+"use strict";
+// ejercicio 1
+// document.querySelector("h1").innerHTML =
+//   "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente";
+
+// // ejercicio 2 (error dado en consola):
+// let myAdress = "calleCuestaDeLaVirgen1";
+// myAdress = "calleSaavedraFajardo2";
+
+// ejercicio 3 (siguiente escrito en Consola)
+// document.querySelector("h1").innerHTML = "hey world";
+
+// ejercicio 4
+// console.log;
+// document.querySelector(".title").innerHTML =
+//   "La adalaber seleccionada es: Alicia";
+
+// ejercicio 5
+// console.log;
+// document.querySelector(".password").innerHTML = "***";
+
+// ejercicio 6
+// console.log;
+// const divElement = document.querySelector("div");
+// const content =
+//   "<h1>Lorem fistrum</h1>" +
+//   "<img src=http://via.placeholder.com/350x150" +
+//   "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p> ";
+// divElement.innerHTML = content;
+
+// ejercicio 7
+// console.log;
+// const listElement = document.querySelector("ul");
+// const content = "<li>1</li>" + "<li>2</li>" + "<li>3</li>";
+// listElement.innerHTML = content;
+
+// ejercicio 8
+// const buttonElement = document.querySelector(".button-2");
+// buttonElement.classList.add("opacity");
